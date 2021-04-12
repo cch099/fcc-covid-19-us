9558aa7c7ecd3263ef035c28ef5f220f5cbabc3c
cch099 <50557026+cch099@users.noreply.github.com>
2021-04-11 19:50:32 -0600

    Changes for skeleton components

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+    import CovidStat from '../components/CovidStat.svelte'
+
+    import CovidChart from '../components/CovidChart.svelte'
+
+    import TableContainer from '../components/TableContainer.svelte'
+</script>
+
+<CovidStat />
+
+<CovidChart />
+
+<TableContainer />

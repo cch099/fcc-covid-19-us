fd4f292fa7464a6f5bc23653170d7f40a820b63f
cch099 <50557026+cch099@users.noreply.github.com>
2021-04-17 15:07:55 -0600

    chart data working

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,32 @@
 <script context="module">
   import requests from "../data/requests.js";
+
   export async function preload() {
     try {
       const usStats = await requests.usStats();
-      return { usStats };
+      const historic = await requests.historicUS();
+      return { usStats, historic };
     } catch (e) {
-      this.error(500, "There was an error in calling the api, please try again in 5 minutes.");
-
+      console.log(e);
+      this.error(
+        500,
+        "There was an error in calling the api, please try again in 5 minutes."
+      );
+      return;
     }
   }
 </script>
 
 <script>
   import CovidStat from "../components/CovidStat.svelte";
+
   import CovidChart from "../components/CovidChart.svelte";
+
   import TableContainer from "../components/TableContainer.svelte";
+
   export let usStats;
+  export let historic;
+  console.log(historic, "historic");
   console.log(usStats, "usStats");
 </script>
 

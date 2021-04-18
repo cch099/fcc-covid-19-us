f57e2f5f080b990040ad4592bce01d94b4a7e2ff
cch099 <50557026+cch099@users.noreply.github.com>
2021-04-18 07:57:33 -0600

    chart working for home page

--- src/data/requests.js
+++ src/data/requests.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 import parsers from './parsers';
 
+// Need reverse proxy from cors-anywhere (that's why we use herokuapp)
 async function usStats() {
   const response = await axios.get(
-    'https://covidtracking.com/api/v1/us/current.json'
+    'https://shrouded-shore-30649.herokuapp.com/https://covidtracking.com/api/v1/us/current.json', {mode: 'no-cors',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+      'origin': 'x-requested-with'
+    }}
   );
 
   return parsers.usStats(response.data);
@@ -11,14 +17,24 @@ async function usStats() {
 
 async function stateStats(state) {
   const response = await axios.get(
-    'https://covidtracking.com/api/v1/states/current.json'
+    'https://shrouded-shore-30649.herokuapp.com/https://covidtracking.com/api/v1/states/current.json', {mode: 'no-cors',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+      'origin': 'x-requested-with'
+    }}
   );
   //console.log("stateStats: " + response.data[0].state);
   return parsers.stateStats(state, response.data);
 }
 
 async function historicUS() {
-  const response = await axios.get('https://covidtracking.com/api/us/daily');
+  const response = await axios.get('https://shrouded-shore-30649.herokuapp.com/https://covidtracking.com/api/us/daily', {mode: 'no-cors',
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+    'origin': 'x-requested-with'
+  }});
 
   return parsers.historicUS(response.data);
 }

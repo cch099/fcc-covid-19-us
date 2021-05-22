bcd98bd211ab8439cbe7e11ba2a4dced0b794949
cch099 <50557026+cch099@users.noreply.github.com>
2021-05-22 15:42:09 -0600

    Changes to gh-pages.js

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
+		'fcc-covid-19-us',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
